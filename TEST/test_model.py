eeaa6dfe06e40c417fa7eb358f6facd1811dd8fa
JoseLizandro <josel.huacanil@gmail.com>
2025-06-15 19:51:29 -0500

    Update test_model.py

--- TEST/test_model.py
+++ TEST/test_model.py
@@ -9,7 +9,7 @@ from src.models.train_model import train_model
 import pandas as pd
 
 def test_model_training():
-    df = pd.read_csv("data/raw/credit_data.csv")
+    df = pd.read_csv("C:/Users/Usuario/Desktop/TRABAJOF/DATA/credit_data.csv")
     X = df[['edad', 'ingresos', 'deuda', 'historia_credito', 'score_buro']]
     y = df['default']
     model = train_model(X, y, output_path="model_test.pkl")
